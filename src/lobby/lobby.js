ab7a6dc3dbcd3d5ca4096bf5b2e76045226f0f77
Leo Orshansky <leoorshansky@gmail.com>
2020-05-02 20:14:07 -0500

    testing

--- src/lobby/lobby.js
+++ src/lobby/lobby.js
@@ -2,4 +2,4 @@ $(document).ready(() => {
     $("#logout").on("click", logout);
 });
 
-const logout = () => window.fetch("/poker/logout");
+const logout = () => window.fetch("/poker/logout", {redirect: 'follow'});
