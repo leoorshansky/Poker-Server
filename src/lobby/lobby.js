b411ef60ed782d18258eb254737773051db5cfb5
Leo Orshansky <leoorshansky@gmail.com>
2020-05-02 20:38:34 -0500

    testing

--- src/lobby/lobby.js
+++ src/lobby/lobby.js
@@ -2,4 +2,4 @@ $(document).ready(() => {
     $("#logout").on("click", logout);
 });
 
-const logout = () => window.fetch("/poker/logout", {credentials: 'include'}).then(window.location.assign("/poker/"));
+const logout = () => window.fetch("/poker/logout", {credentials: 'include'}).then(() => window.location.assign("/poker/"));
