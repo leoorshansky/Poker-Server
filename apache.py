44ca43346bbe644583ace5b232e82b6370bd58b8
Leo Orshansky <leoorshansky@gmail.com>
2020-07-18 14:53:17 -0500

    fixed reverse proxy

--- apache.py
+++ apache.py
@@ -1,7 +1,7 @@
 from flask import Flask, flash, render_template, redirect, url_for, session, request, send_from_directory, abort
 from flask import Response, Blueprint
 from werkzeug.serving import run_simple
-from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 import datetime
 
 class ReverseProxied(object):
