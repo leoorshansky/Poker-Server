2ffe9e7323dd2f83405addb228cda85e6a2ba91b
Leo Orshansky <leoorshansky@gmail.com>
2020-07-18 14:32:25 -0500

    updates

--- src/lobby/lobby.js
+++ src/lobby/lobby.js
@@ -1,5 +1,14 @@
 $(document).ready(() => {
     $("#logout").on("click", logout);
+    getAvatar();
 });
 
 const logout = () => window.fetch("/poker/logout", {credentials: 'include'}).then(() => window.location.assign("/poker/"));
+const getAvatar = async () => {
+    const res = await window.fetch("/poker/avatar", {credentials: 'include'});
+    if (res.ok){
+        $("#avatar").prop("src", await res.text())
+        return
+    }
+    $("#avatar").css("display", "none");
+};
