0bc8c59ebd2f23e6bcce4f97c5447097b1957130
Leo Orshansky <leoorshansky@gmail.com>
2020-08-18 17:45:48 -0500

    fixing stuff

--- poker_server/poker.py
+++ poker_server/poker.py
@@ -350,7 +350,7 @@ class Poker(socketio.AsyncNamespace):
 			await sio.disconnect(sid)
 			return
 		self.users = self.users.append(username)
-		print(self.users)
+		print(self.users, username)
 		async with sio.session(sid) as session:
 			session['username'] = username
 
